570e7fe7ddadca18c447322269e6f65b2010c1df
odadzhiev <boris.s.odadzhiev.2022@elsys-bg.org>
2026-03-27 15:49:49 +0200

    updating password model

--- website/models.py
+++ website/models.py
@@ -1,19 +1,18 @@
 from . import db
-from datetime import datetime
 from flask_login import UserMixin
 
-class HealthData(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
-    pulse = db.Column(db.Integer)
-    temperature = db.Column(db.Float)
-    movement    = db.Column(db.Boolean, default=False)
-    state = db.Column(db.String(50))
-    device_id = db.Column(db.String(50))
-    
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(120), unique=True, nullable=False)
-    device_id = db.Column(db.String(50),  nullable=True)
+    password = db.Column(db.String(255), nullable=False) # Removed unique=True
+    device_id = db.Column(db.String(50), nullable=True)
+
+class HealthData(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.DateTime, default=db.func.now())
+    pulse = db.Column(db.Integer)
+    temperature = db.Column(db.Float)
+    movement = db.Column(db.Boolean, default=False)
+    state = db.Column(db.String(50))
+    device_id = db.Column(db.String(50))
 
