90bcdad7ba41a8a1741e7935526acc97d1ca92f6
MayaKKoleva <161861698+MayaKKoleva@users.noreply.github.com>
2026-03-27 18:20:22 +0200

    adding libraries

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, jsonify
+import requests
 from flask_login import login_required, current_user
 from website import create_app, db
 from website.models import HealthData
