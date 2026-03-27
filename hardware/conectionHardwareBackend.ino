d42b97f305b8c0dbe8c429e7e79ee30bb868c52d
bettyvaxsdr <elizabet.e.daskalova.2022@elsys-bg.org>
2026-03-27 02:52:52 +0200

    Test: connection between backend and hardware
    Testing the connection between ESP32 and backend server.

---
+++ hardware/conectionHardwareBackend.ino
@@ -0,0 +1,70 @@
+#include <WiFi.h>
+#include <WebServer.h>
+#include <ArduinoJson.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+
+const char* ssid = "A1-B122";
+const char* password = "83CC22BCA5";
+
+const int oneWireBus = 25; 
+OneWire oneWire(oneWireBus);
+DallasTemperature sensors(&oneWire);
+
+WebServer server(80);
+
+void handleDataRequest() {
+  sensors.requestTemperatures();
+  float tempC = sensors.getTempCByIndex(0);
+
+  JsonDocument doc; 
+  
+  if (tempC != DEVICE_DISCONNECTED_C) {
+    doc["sensor"] = "DS18B20";
+    doc["value"] = tempC;
+    doc["unit"] = "Celsius";
+    doc["status"] = "OK";
+  } else {
+    doc["sensor"] = "DS18B20";
+    doc["value"] = 0;
+    doc["unit"] = "Celsius";
+    doc["status"] = "Error: Sensor not found";
+  }
+
+  String response;
+  serializeJson(doc, response);
+  
+  server.send(200, "application/json", response);
+  
+  Serial.print("Sent temperature: ");
+  Serial.println(tempC);
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("DS18B20 Temperature Sensor Test");
+  sensors.begin();
+
+
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("\nWiFi connected.");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP()); //за Flask кода
+
+  // Деф. endpoint
+  server.on("/data", HTTP_GET, handleDataRequest);
+
+  server.begin();
+  Serial.println("HTTP Server started");
+}
+
+void loop() {
+  server.handleClient();
+}
