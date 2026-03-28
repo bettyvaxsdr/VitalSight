5b2ee4c780ba7add5fd8804664b5fb955beb5bd2
MayaKKoleva <161861698+MayaKKoleva@users.noreply.github.com>
2026-03-28 02:46:09 +0200

    frontend data

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import jsonify, render_template
+from flask import jsonify, render_template, request
 from flask_login import login_required, current_user
 from website import create_app, db
 from website.models import HealthData
@@ -13,40 +13,42 @@ ESP32_IP = "192.168.8.75"
 def index():
     return render_template('home.html', user=current_user)
 
-@app.route('/api/status')
-@login_required
-def get_status():
+@app.route('/sensor-data', methods=['POST'])
+def sensor_data():
+    res = request.get_json()
+    print("Received data:", res)
     try:
-        res  = requests.get(f"http://{ESP32_IP}/data", timeout=2)
-        data = res.json()
+        db.session.add(HealthData(
+        pulse=res.get("bpm"),
+        temperature=res.get("value"),
+        movement=res.get("movement", False),
+        state=res.get("state", ""),
+        device_id=res.get("sensor")
+        ))
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error": str(e)}), 500
+    return jsonify({"status": "data received"}), 200
 
-        pulse       = data.get("pulse")
-        temperature = data.get("temperature")
-        movement    = data.get("movement")
-        state       = data.get("state")
+@app.route('/latest-data', methods=['GET'])
+@login_required
+def latest_data():
+    try:
+        data = HealthData.query.order_by(HealthData.id.desc()).first()
 
-        alarm = False
-        if pulse       is not None and (pulse < 40 or pulse > 120):
-            alarm = True
-        if temperature is not None and (temperature < 35 or temperature > 38):
-            alarm = True
-        if movement:
-            alarm = True
+        if not data:
+            return jsonify({"error": "No data"}), 404
 
         return jsonify({
-            "pulse":       pulse,
-            "temperature": temperature,
-            "movement":    movement,
-            "state":       state,
-            "alarm":       alarm
+            "pulse": data.pulse,
+            "temperature": data.temperature,
+            "movement": data.movement,
+            "state": data.state
         })
 
     except Exception as e:
-        return jsonify({
-            "error":   "ESP32 not reachable",
-            "message": str(e)
-        }), 500
-
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=5000)
